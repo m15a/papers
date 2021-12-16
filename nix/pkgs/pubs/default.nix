808da5fd6d9a0c150acbb801cfbabe719b911309
Mitsuhiro Nakamura <mitsuhiro_nakamura@albert2005.co.jp>
2021-12-16 15:50:16 +0900

    chore: move pubs derivation to nix/pkgs

---
+++ nix/pkgs/pubs/default.nix
@@ -0,0 +1,30 @@
+{ pubs, fetchFromGitHub, installShellFiles, argcomplete }:
+
+pubs.overridePythonAttrs (old: {
+  version = "2021-11-22";
+
+  src = fetchFromGitHub {
+    owner = "pubs";
+    repo = "pubs";
+    rev = "4ec4ba0390ef6176687326f2f8b5a2c923dfa47b";
+    sha256 = "sha256-0y6dXgkFZYVsbDlYX+FEhChQRPXhwNHNPCrYQNcWclo=";
+  };
+
+  buildInputs = (old.buildInputs or []) ++ [
+    installShellFiles
+  ];
+
+  patches = (old.patches or []) ++ [ ./pubs.patch ];
+
+  postInstall = (old.postInstall or "") + ''
+    register="${argcomplete}/bin/register-python-argcomplete"
+    "$register" --shell bash pubs > pubs.bash
+    "$register" --shell fish pubs > pubs.fish
+    installShellCompletion --bash --name pubs.bash pubs.bash
+    installShellCompletion --fish --name pubs.fish pubs.fish
+  '';
+
+  meta = old.meta // {
+    description = old.meta.description + " (patched: https://github.com/pubs/pubs/pull/273)";
+  };
+})
